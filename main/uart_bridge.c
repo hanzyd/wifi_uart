7475a354668e5a5f2f1a46b72b294e068f7529a3
Ivan T. Ivanov <iivanov@suse.de>
2022-10-22 19:55:04 +0300

    Merge to one file

--- main/uart_bridge.c
+++ main/uart_bridge.c
@@ -6,18 +6,48 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include "freertos/FreeRTOS.h"
-#include "driver/uart.h"
-#include "esp_event.h"
+
 #include <string.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "driver/uart.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
+
+#include "nvs_flash.h"
+
 #include "lwip/sockets.h"
+
+
+/* The examples use WiFi configuration that you can set via project
+   configuration menu
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
+
+
+/* The event group allows multiple bits for each event, but we only care about
+ * two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
 
 #define UART_BUF_SIZE 1024
 #define SRV_PORT 8888
 
+static int g_retry_num = 0;
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t g_wifi_events;
 static QueueHandle_t uart_queue;
-
 static char rx_buff[128];
 static uint8_t tx_buff[128];
 
@@ -27,12 +57,9 @@ static int init_wifi_server(int backlog)
 	int srv_sock;
 
 	srv_addr.sin_family = AF_INET;
-	// Grabs connected ip address which is set by the wifi_init_sta
 	srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	srv_addr.sin_port = htons(SRV_PORT);
 
-	// initialize tcp socket AF_INET refers to IPV4 addresses and
-	// SOCK_STREAM saids initiate socket as tcp socket.
 	srv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 	if (srv_sock < 0)
 		return -1;
@@ -213,10 +240,88 @@ static void read_uart_send_wifi_task(void *arg)
 	vTaskDelete(NULL);
 }
 
-void bridge_task(void *pvParameters)
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+							   int32_t event_id, void *event_data)
+{
+	switch (event_id) {
+	case WIFI_EVENT_STA_START:
+		esp_wifi_connect();
+		break;
+
+	case WIFI_EVENT_STA_DISCONNECTED:
+		if (g_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			g_retry_num++;
+		} else {
+			xEventGroupSetBits(g_wifi_events, WIFI_FAIL_BIT);
+		}
+		break;
+	}
+}
+
+static void ip_event_handler(void *arg, esp_event_base_t event_base,
+							 int32_t event_id, void *event_data)
+{
+	switch (event_id) {
+	case IP_EVENT_STA_GOT_IP:
+		g_retry_num = 0;
+		xEventGroupSetBits(g_wifi_events, WIFI_CONNECTED_BIT);
+		break;
+	}
+}
+
+static bool init_wifi_station_and_connect(void)
+{
+	g_wifi_events = xEventGroupCreate();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	esp_wifi_init(&cfg);
+
+	esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+							   &wifi_event_handler, NULL);
+	esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler,
+							   NULL);
+
+	wifi_config_t wifi_config = {
+		.sta = {.ssid = EXAMPLE_ESP_WIFI_SSID,
+				.password = EXAMPLE_ESP_WIFI_PASS},
+	};
+
+	/* Setting a password implies station will connect to all security modes
+	 * including WEP/WPA. However these modes are deprecated and not advisable
+	 * to be used. Incase your Access point doesn't support WPA2, these mode can
+	 * be enabled by commenting below line */
+
+	if (strlen((char *)wifi_config.sta.password)) {
+		wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+	}
+
+	esp_wifi_set_mode(WIFI_MODE_STA);
+	esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
+	esp_wifi_start();
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT)
+	 * or connection failed for the maximum number of re-tries (WIFI_FAIL_BIT).
+	 * The bits are set by event_handler() (see above) */
+	EventBits_t bits = xEventGroupWaitBits(g_wifi_events,
+										   WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+										   pdFALSE, pdFALSE, portMAX_DELAY);
+
+	if (bits & WIFI_CONNECTED_BIT)
+		return true;
+
+	return false;
+}
+
+static void bridge_task(void *pvParameters)
 {
 	int client = -1;
 	int srv_sock;
+
+	bool ok = init_wifi_station_and_connect();
+	if (!ok)
+		vTaskDelete(NULL);
 
 	srv_sock = init_wifi_server(2); // Initial server configuration.
 	if (srv_sock < 0)
@@ -241,4 +346,13 @@ void bridge_task(void *pvParameters)
 
 		client = new_client;
 	}
+}
+
+void app_main()
+{
+	nvs_flash_init();
+    esp_netif_init();
+    esp_event_loop_create_default();
+
+	xTaskCreate(bridge_task, "bridge_task", 1024 * 2, NULL, 2, NULL);
 }
