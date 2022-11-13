40ec384a758960c1bb31e4c7ebb7358f88c87a2f
Ivan T. Ivanov <iivanov@suse.de>
2022-11-13 11:03:32 +0200

    Send feedback during update process

--- main/ota_upgrade.c
+++ main/ota_upgrade.c
@@ -54,20 +54,24 @@ esp_err_t upgrade_endpoint(httpd_req_t *req)
 	const esp_partition_t *partition = NULL;
 	int ret, remaining = req->content_len;
 	const char *resp_str;
-	esp_err_t werr = ESP_OK;
-	esp_err_t err = ESP_FAIL;
+	esp_err_t err;
 
 	partition = esp_ota_get_next_update_partition(NULL);
 	if (partition == NULL) {
 		resp_str = "Can't find partition\n";
-		goto exit;
+		httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
+		return ESP_FAIL;
 	}
 
 	err = esp_ota_begin(partition, remaining, &ota_handle);
 	if (err != ESP_OK) {
 		resp_str = "Start OTA failed\n";
-		goto exit;
+		httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
+		return ESP_FAIL;
 	}
+
+	resp_str = "Collecting OTA parts ";
+	httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
 
 	while (remaining > 0) {
 
@@ -78,29 +82,32 @@ esp_err_t upgrade_endpoint(httpd_req_t *req)
 				/* Retry receiving if timeout occurred */
 				continue;
 			}
+			resp_str = "Receive error\n";
+			httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
 			return ESP_FAIL;
 		}
 
-		werr = esp_ota_write(ota_handle, (const void *)up_buf, ret);
-		if (werr != ESP_OK) {
+		err = esp_ota_write(ota_handle, (const void *)up_buf, ret);
+		if (err != ESP_OK) {
 			resp_str = "Write OTA error\n";
-			break;
-		} else {
-			resp_str = "";
-		}
+			httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
+			return ESP_FAIL;
+		} 
+
+		remaining -= ret;
+		if (remaining)
+			resp_str = ".";
+		else
+			resp_str = " complete\n";
 
 		httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
-		remaining -= ret;
 	}
 
 	err = esp_ota_end(ota_handle);
-
-	if (werr != ESP_OK)
-		err = werr;
-
 	if (err != ESP_OK) {
 		resp_str = "Finish OTA failed\n";
-		goto exit;
+		httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
+		return ESP_FAIL;
 	}
 
 	err = esp_ota_set_boot_partition(partition);
@@ -111,7 +118,6 @@ esp_err_t upgrade_endpoint(httpd_req_t *req)
 		resp_str = "Can't set boot partition\n";
 	}
 
-exit:
 	httpd_resp_send_chunk(req, resp_str, strlen(resp_str));
 
 	// End response
