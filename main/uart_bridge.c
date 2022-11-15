58a998dd3a42f7c0b1a7caa209a8f9de736af014
Ivan T. Ivanov <iivanov@suse.de>
2022-11-15 21:06:50 +0200

    Move low level WiFi client handling code

--- main/uart_bridge.c
+++ main/uart_bridge.c
@@ -14,7 +14,6 @@
 #include "freertos/event_groups.h"
 
 #include "driver/uart.h"
-#include "esp_event.h"
 #include "esp_wifi.h"
 #include "esp_netif.h"
 
@@ -23,30 +22,9 @@
 #include "lwip/sockets.h"
 
 
-/* The examples use WiFi configuration that you can set via project
-   configuration menu
-
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_ESP_WIFI_SSID CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
-#define EXAMPLE_ESP_MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
-
-
-/* The event group allows multiple bits for each event, but we only care about
- * two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT BIT1
-
 #define UART_BUF_SIZE 1024
 #define SRV_PORT 8888
 
-static int g_retry_num = 0;
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t g_wifi_events;
 static QueueHandle_t uart_queue;
 static char rx_buff[128];
 static uint8_t tx_buff[128];
@@ -240,90 +218,8 @@ static void read_uart_send_wifi_task(void *arg)
 	vTaskDelete(NULL);
 }
 
-
-static void wifi_event_handler(void *arg, esp_event_base_t event_base,
-							   int32_t event_id, void *event_data)
-{
-	switch (event_id) {
-	case WIFI_EVENT_STA_START:
-		esp_wifi_connect();
-		break;
-
-	case WIFI_EVENT_STA_DISCONNECTED:
-		if (g_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-			esp_wifi_connect();
-			g_retry_num++;
-		} else {
-			xEventGroupSetBits(g_wifi_events, WIFI_FAIL_BIT);
-		}
-		break;
-	default:
-		break;
-	}
-}
-
-static void ip_event_handler(void *arg, esp_event_base_t event_base,
-							 int32_t event_id, void *event_data)
-{
-	switch (event_id) {
-	case IP_EVENT_STA_GOT_IP:
-		g_retry_num = 0;
-		xEventGroupSetBits(g_wifi_events, WIFI_CONNECTED_BIT);
-		break;
-	default:
-		break;
-	}
-}
-
-
 void httpd_register_for_events(void);
-
-static bool init_wifi_station_and_connect(void)
-{
-	wifi_config_t user_config, *config;
-	esp_err_t sta;
-
-	g_wifi_events = xEventGroupCreate();
-
-	esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-							   &wifi_event_handler, NULL);
-	esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler,
-							   NULL);
-
-
-	wifi_config_t factory_config = {
-		.sta = {
-			.ssid = EXAMPLE_ESP_WIFI_SSID,
-			.password = EXAMPLE_ESP_WIFI_PASS,
-			.threshold.authmode = WIFI_AUTH_WPA2_PSK
-		},
-	};
-
-	esp_wifi_set_storage(WIFI_STORAGE_FLASH);
-	esp_wifi_set_mode(WIFI_MODE_STA);
-
-	sta = esp_wifi_get_config(ESP_IF_WIFI_STA, &user_config);
-	if (sta == ESP_OK)
-		config = &user_config;
-	else
-		config = &factory_config;
-
-	esp_wifi_set_config(ESP_IF_WIFI_STA, config);
-	esp_wifi_start();
-
-	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT)
-	 * or connection failed for the maximum number of re-tries (WIFI_FAIL_BIT).
-	 * The bits are set by event_handler() (see above) */
-	EventBits_t bits = xEventGroupWaitBits(g_wifi_events,
-										   WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-										   pdFALSE, pdFALSE, portMAX_DELAY);
-
-	if (bits & WIFI_CONNECTED_BIT)
-		return true;
-
-	esp_wifi_stop();
-	return false;
-}
+bool init_wifi_station_and_connect(void);
 
 static void bridge_task(void *pvParameters)
 {
